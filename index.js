83b432123e40e37931f25cfd516fac4f2915500a
rkrukshan <rukshan1122@gmail.com>
2026-07-28 15:58:10 +0530

    Fix server response message and update port number in index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Server is Responded");
+  res.write("server is started");
   res.end();
 });
 
-const port = 3001;
+const port = 5173;
 
 server.listen(port, () => {
-  console.log(`server is running in port ${port}`);
+  console.log(`server is running on ${port}`);
 });
