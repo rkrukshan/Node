a9ae5207f24d32af5373b3c0af7dea77c315e12f
rkrukshan <rukshan1122@gmail.com>
2026-07-28 16:16:30 +0530

    Update server response messages and change port number in index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("server is started");
+  if (req.url === "/") {
+    res.write("it's home page");
+  } else if (req.url === "/about") {
+    res.write("it's about page");
+  } else {
+    res.write("page not found");
+  }
   res.end();
 });
 
-const port = 5173;
+const port = 3000;
 
 server.listen(port, () => {
-  console.log(`server is running on ${port}`);
+  console.log(`server is running on port ${port}`);
 });
